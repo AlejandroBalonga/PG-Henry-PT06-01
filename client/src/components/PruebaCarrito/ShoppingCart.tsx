cd4b6ff6488d932b058d0f333509614bf7978385
herinyhert <herinyhert@gmail.com>
2022-09-10 01:04:06 -0300

    se deja funcional el incremental y desminuir cantidades, al igual que renderizar su respetivo costo

--- client/src/components/PruebaCarrito/ShoppingCart.tsx
+++ client/src/components/PruebaCarrito/ShoppingCart.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Link } from "react-router-dom";
-import { Articulo } from "../../actions";
+import { Articulo, ArticuloCarrito } from "../../actions";
 import NavBar from "../../components/NavBar/NavBar";
 import { ReduxState } from "../../reducer";
 
@@ -23,23 +23,22 @@ import {
   DivUnidad,
 } from "./stylesCart";
 
-
-export default function ShoppingCart() { 
-  
+export default function ShoppingCart() {
   let detail = useSelector((state: ReduxState) => state.detailsProduct);
 
-  let detalle : Articulo =   {
+  let detalle: ArticuloCarrito = {
     id: detail?.id,
-    name:detail?.name ,
-    brand:detail?.brand ,
-    stock:detail?.stock ,
-    price:detail?.price ,
-    img:detail?.img ,
-    state:detail?.state ,
-    categoryId:detail?.categoryId ,
-    category:detail?.category ,
+    name: detail?.name,
+    brand: detail?.brand,
+    stock: detail?.stock,
+    price: detail?.price,
+    img: detail?.img,
+    state: detail?.state,
+    categoryId: detail?.categoryId,
+    category: detail?.category,
     totalCount: 1,
-   }
+    precioTotal: detail?.price,
+  };
 
   let preciofinal = 0;
   let productosCarrito = JSON.parse(localStorage.getItem("carrito"));
@@ -49,38 +48,58 @@ export default function ShoppingCart() {
     productosCarrito = [];
   }
 
-  const [articulo, setArticulo] = useState([productosCarrito])
+  const [articulo, setArticulo] = useState([productosCarrito]);
 
-  function handlerPrecioCantidad(detalle){
-    console.log(detalle)
-    
-    setArticulo(detalle)
-    
-    const index = productosCarrito.findIndex((art) => art.id === detalle.id)
-    let precioUnitario = detail.price
-    
-    let carritoAux = JSON.parse(localStorage.getItem('carrito'));
-      carritoAux[index].totalCount = carritoAux[index].totalCount + 1;
-      //carritoAux[index].price = carritoAux[index].totalCount * precioUnitario
-      localStorage.setItem('carrito', JSON.stringify(carritoAux))
+  function handlerPrecioCantidadSuma(detalle) {
+    //console.log(detalle)
+    setArticulo(detalle);
+
+    const index = productosCarrito.findIndex((art) => art.id === detalle.id);
+    let precioUnitario = detail.price;
+
+    let carritoAux = JSON.parse(localStorage.getItem("carrito"));
+    carritoAux[index].totalCount = carritoAux[index].totalCount + 1;
+    carritoAux[index].precioTotal =
+      carritoAux[index].price * carritoAux[index].totalCount;
+    localStorage.setItem("carrito", JSON.stringify(carritoAux));
   }
 
-  function handlerPrecioCantidadMenos(detalle){
-    setArticulo(detalle)
+  function handlerPrecioCantidadResta(detalle) {
+    setArticulo(detalle);
 
-    const index = productosCarrito.findIndex((art) => art.id === detalle.id)
-    let precioUnitario = detail.price
-    
-    let carritoAux = JSON.parse(localStorage.getItem('carrito'));
-      carritoAux[index].totalCount = carritoAux[index].totalCount - 1;
-      carritoAux[index].price = carritoAux[index].price - precioUnitario
-      localStorage.setItem('carrito', JSON.stringify(carritoAux))
+    const index = productosCarrito.findIndex((art) => art.id === detalle.id);
+    let precioUnitario = detail.price;
 
-    }
-    const index = productosCarrito?.findIndex((art) => art.id === detalle.id)
-    const controllerDisabledButon = productosCarrito[index]?.totalCount <= 1 ? true : false
+    let carritoAux = JSON.parse(localStorage.getItem("carrito"));
+    carritoAux[index].totalCount = carritoAux[index].totalCount - 1;
+    carritoAux[index].precioTotal =
+      carritoAux[index].price * carritoAux[index].totalCount;
+    localStorage.setItem("carrito", JSON.stringify(carritoAux));
+  }
 
- 
+  function handlerDelete(detalle) { //en esta funcion defino el poder elimiar un articulo de carrito
+    setArticulo(detalle);
+
+//me guardo lo que tengo en LocalStorage(LS) en una variable
+    let carritoDelete = JSON.parse(localStorage.getItem("carrito"));
+//creo otra variable para recorrer la posicion dentro de la variable anterior
+    let carritoIndex = carritoDelete.findIndex((el)=> el.id === detalle.id);
+// utlizo el metodo splice para eliminar el elemento encontrado
+    carritoIndex.splice(carritoIndex, 1)
+    //console.log("quiero eliminar este", carritoIndex)
+//cargo de nuevo al LS la variable con la nueva informacion
+    localStorage.setItem("carrito", JSON.stringify(carritoDelete));
+
+    //en resumidas cuentas esto es lo que hago paso a paso, me esta dando error ya que me dice que no reconoce el metodo splice, espero que con esto les sirva
+    //para orientar y puedan sacarlo, los veo a la noche cuando vuelva, les deseo un hermoso y maravilloso día, y que diosito les de bastante sabiduria para 
+    //sacar este problema adelante
+  }
+
+
+  const index = productosCarrito?.findIndex((art) => art.id === detalle.id);
+  const controllerDisabledButon =
+    productosCarrito[index]?.totalCount <= 1 ? true : false;
+
   return (
     <>
       <NavBar />
@@ -93,25 +112,32 @@ export default function ShoppingCart() {
           <Column>
             <DivTitulo>
               <h3>Mi CARRITO ({productosCarrito?.length})</h3>
-              
             </DivTitulo>
 
             {productosCarrito?.map((p, i) => (
-              
               <DivProduct key={i}>
                 <DivUnidad>
                   <img src={p.img} alt="img" width="80px" />
                   <h3>{p.name}</h3>
-                  <Cantidad type="number" min="1" value={p.totalCount}></Cantidad>
-                  <Unidad>${p.price?.toFixed(2)}</Unidad>
+                  <ButtonComprar onClick={() => handlerPrecioCantidadSuma(p)}>
+                    +
+                  </ButtonComprar>
+                  <h3>{p.totalCount}</h3>
+                  {/* <Cantidad value={p.totalCount}></Cantidad> */}
+                  <ButtonComprar
+                    /*disabled={controllerDisabledButon}*/ onClick={() =>
+                      handlerPrecioCantidadResta(p)
+                    }
+                  >
+                    -
+                  </ButtonComprar>
+                  <Unidad>${p.price?.toFixed(2)}</Unidad>unidad
+                  <Unidad>${p.precioTotal?.toFixed(2)}</Unidad>total
                   {/* //<p>{p.price * p.totalCount}</p> */}
-                  <ButtonComprar  onClick={()=>handlerPrecioCantidad(p)}>+</ButtonComprar>
-                  
-                  {<ButtonComprar disabled={controllerDisabledButon} onClick={()=>handlerPrecioCantidadMenos(p)}>-</ButtonComprar>}
-
+                  {/* <ButtonComprar  onClick={()=>handlerPrecioCantidadSuma(p)}>+</ButtonComprar> */}
                 </DivUnidad>
                 <Decision>
-                  <ButtonComprar>Delete</ButtonComprar>
+                  <ButtonComprar onClick={() => handlerDelete(p)}>Delete</ButtonComprar>
 
                   <ButtonComprar>Comprar ahora</ButtonComprar>
                 </Decision>
